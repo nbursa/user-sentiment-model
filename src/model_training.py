a55e106a6ad6dbcf5d5831ac4a34e9860ff98dfb
Nenad <nbursa@gmail.com>
2024-06-25 02:14:06 +0200

    Model training

---
+++ src/model_training.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.utils import to_categorical
+
+
+def train_model(processed_data_path, model_path):
+    data = pd.read_csv(processed_data_path)
+    data['sentiment'] = data['sentiment'].map({'Negative': 0, 'Positive': 1})
+    X = data['clean_text']
+    y = data['sentiment']
+
+    tokenizer = Tokenizer(num_words=10000)
+    tokenizer.fit_on_texts(X)
+    X_seq = tokenizer.texts_to_sequences(X)
+    X_pad = pad_sequences(X_seq, maxlen=100)
+
+    y_cat = to_categorical(y)
+    X_train, X_test, y_train, y_test = train_test_split(X_pad, y_cat, test_size=0.2, random_state=42)
+
+    model = Sequential()
+    model.add(Embedding(input_dim=10000, output_dim=128, input_length=100))
+    model.add(LSTM(units=128, dropout=0.2, recurrent_dropout=0.2))
+    model.add(Dense(2, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    early_stopping = EarlyStopping(monitor='val_loss', patience=3)
+
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=128, callbacks=[early_stopping])
+
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
+    model.save(model_path)
+    print("Model saved to", model_path)
